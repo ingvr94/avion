cf76c78c4b71824ec169e0cf518d06a98a981316
ingvr94 <ingvar94@mail.ru>
2024-06-23 17:05:54 +0300

    all products bg img

--- src/pages/ProductListings.tsx
+++ src/pages/ProductListings.tsx
@@ -7,7 +7,6 @@ import { useState} from "react"
 import useMediaQuery from '@/hooks/useMediaQuery'
 import { XMarkIcon } from "@heroicons/react/24/solid"
 import { Link } from 'react-router-dom'
-import PageHeader from '@/assets/Page Headers-2.png'
 
 
 type ProductListingsType ={
@@ -32,8 +31,7 @@ const ProductListings = ({categories,cost,designers,onFilterChange,modalActive,s
     <div className="container">
       <Navbar />
       <div 
-      style={{backgroundImage:`url(${PageHeader})`}}
-      className="md:h-[209px] xs:h-[146px] xs:mt-[80px] md:mt-0 xs:pt-16 xs:pb-8 xs:px-[92px] md:pl-20 md:pt-[123px]">  
+      className=" bg-page-header md:h-[209px] xs:h-[146px] xs:mt-[80px] md:mt-0 xs:pt-16 xs:pb-8 xs:px-[92px] md:pl-20 md:pt-[123px]">  
         <div className=" text-h1 font-clashdisplay  text-white">All products</div>
       </div>
       {isAboveMediumScreen ?
